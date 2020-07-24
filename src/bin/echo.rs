a1887d7d92a82ea39343cbaa7f14bd0c5ff52394
James Bostock <james.bostock@gmail.com>
2020-07-25 08:25:35 +1000

    Echo: support the -n command line option.

--- src/bin/echo.rs
+++ src/bin/echo.rs
@@ -4,15 +4,40 @@
 // An implementation of the echo(1) command in Rust.
 // See http://man.cat-v.org/unix-7th/1/echo
 
+use std::env;
+
+use rust_v7_lib as lib;
+
 fn main() {
+    let mut args = env::args();
+    let prog = args.next().unwrap();
+    let getopt = lib::GetOpt::new("n", args);
     let mut first = true;
-    for arg in std::env::args().skip(1) {
-	if first {
-	    first = false;
-	} else {
-	    print!(" ");
+    let mut newline = true;
+
+    for optarg in getopt {
+        match optarg {
+            Ok(lib::Arg::Opt('n')) => newline = false,
+	    Ok(lib::Arg::Arg(arg)) => {
+		if first {
+		    first = false;
+		} else {
+		    print!(" ");
+		}
+		print!("{}", arg);
+	    },
+            Ok(val) => {
+                eprintln!("{}: error: unexpected: {:?}", prog, val);
+                std::process::exit(1);
+            },
+            Err(e) => {
+                eprintln!("{}: error: {}", prog, e);
+                std::process::exit(1);
+            }
 	}
-        print!("{}", arg);
     }
-    println!();
+
+    if newline {
+	println!();
+    }
 }
